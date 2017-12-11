65ec9a3db2e1bac3b6de83a10573d2abb82ace65
WildcherryBazzaire <Eieghtbitdolphin@gmail.com>
2017-12-11 10:51:36 -1000

    Ich bin das Walroß

--- magic.js
+++ magic.js
@@ -34,3 +34,55 @@ var animalHouse = function(k,l) {
 }
 animalHouse(23,46);
 console.log(count);
+
+//1 fish 2 fish countinued 
+if(count > 2)
+  console.log("We have many Elephants");
+else
+  console.log("Welcome to the Jungle");
+
+//Assembling the Avengers
+var hero = "Muammar Gaddafi";
+
+var avengersAssemble = function(avenger) {
+  if(avenger == "Iron Man")
+    console.log("Man in Tin Can");
+  else if(avenger == "Hulk")
+    console.log("Big Green Pea");
+  else if(avenger == "Black Widow")
+    console.log("Important love interest")
+  else if(avenger == "Hawkeye")
+    console.log("Hawk Garbage");
+  else if(avenger == "Muammar Gaddafi")
+    console.log("Mad Dog of the Middle East");
+}
+
+avengersAssemble(hero);
+
+//lights out baby
+
+var lights = true;
+
+if(lights)
+  console.log("is this what you wanted to see?")
+else
+  console.log("No more bright lights...");
+
+//Vroom vroom
+var carType = "Lotus";
+color = "Honey Yellow";
+
+var shipment = (car,color) => {
+  car.toLowerCase();
+  color.toLowerCase();
+  if(car + " " + color == "blue toyota")
+    console.log("We have your " + color + " " + car);
+  else if(car + " " + color == "black tesla")
+    console.log("We have your " + color + " " + car);
+  else if(car + " " + color == "red lamborgini")
+    console.log("We have your " + color + " " + car);
+  else
+    console.log("We have your " + color + " " + car);
+}
+
+shipment(carType,color);
